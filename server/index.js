09067279b7e0abee941a9c760bff2f1df8f18760
ahmadali3180 <mahmadaliwork3180@gmail.com>
2023-10-01 01:37:12 +0500

    minor fixes

--- server/index.js
+++ server/index.js
@@ -10,11 +10,7 @@ dotenv.config();
 
 const app = express();
 
-const corsOptions = {
-  origin: 'https://ravalbit-ai-art-server.onrender.com/', // Replace with your React app's domain
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/dalle", dalleRoutes);
